6496978d744fe047a44c9b6aa8421b5705e3be18
HignyJo <92925129+HignyJo@users.noreply.github.com>
2021-12-21 14:02:56 +0100

    Classe Main & MenuPrincipale

---
+++ src/menuPrincipale.java
@@ -0,0 +1,52 @@
+import Stagiaire.stagiaire;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class menuPrincipale {
+
+    static void principaleMenu() {
+
+        int choix = -1;
+
+        while (choix > 3 || choix < 0) {
+
+            System.out.println("| Outil de gestion de stage |");
+            System.out.println("---    Menu principale    ---");
+            System.out.println("1. Gestion des horaires");
+            System.out.println("2. Gestion des inscriptions");
+            System.out.println("3. Gestion des activités");
+            System.out.println("-----------------------------");
+            System.out.println("0. QUITTER");
+            System.out.println("-----------------------------");
+            System.out.println("Votre choix : ");
+
+            Scanner sc = new Scanner(System.in);
+            choix = sc.nextInt();
+
+            switch (choix) {
+                case 1:
+                    gestionHoraires.menuHoraires();
+                    break;
+
+                case 2:
+                    gestionsInscriptions.menuInscription();
+                    break;
+
+                case 3:
+                    gestionActivites.menuActivites();
+                    break;
+
+                case 0:
+                    System.out.println("Vous avez décidé de quitter, au revoir !");
+                    break;
+
+                default:
+                    System.out.println("Erreur lors du choix !");
+
+            }
+        }
+
+    }
+}
